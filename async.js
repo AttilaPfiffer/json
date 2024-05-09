3f49e1efde2f394e5b6f7ff52450e55d4721a06d
AttilaPfiffer <pifuati@gmail.com>
2024-05-09 09:23:52 +0200

    json

---
+++ async.js
@@ -0,0 +1,7 @@
+
+export function getKeres(vegpont, callback) {
+    fetch(vegpont)
+    .then((response) => response.json())
+    .then((data) => (callback(data)))
+    .catch((error) => (console.log(error)))
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+import { getKeres } from "./async.js"
+
+console.log("1. üzenet")
+
+getKeres("adatok.json", megjelenit1)
+
+getKeres("https://www.boredapi.com/api/activity", megjelenit1)
+
+console.log("2. üzenet")
+
+function megjelenit1(adat) {
+    console.log(adat)
+}
+
+function megjelenitBored(adat) {
+    $("body").append
+}
